4e6bdd4da62807f23baa8f5b97ccc217e71537b3
ahmad <eng.ahmed.anwar@gmail.com>
2021-08-27 14:03:43 +1200

    remove db functions, seeds, migration and routes for teams and themese

--- server/server.js
+++ server/server.js
@@ -1,16 +1,12 @@
 const express = require('express')
 const path = require('path')
 
-const teams = require('./routes/teams')
-const themes = require('./routes/themes')
 const people = require('./routes/people')
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
-server.use('/api/v1/teams', teams)
-server.use('/api/v1/themes', themes)
 server.use('/api/v1/people', people)
 
 module.exports = server
