e8e42ce01d284828296df841ff9b890952fff7a9
ahmad <eng.ahmed.anwar@gmail.com>
2021-08-28 06:17:30 +1200

    refactor people seed

--- server/db/seeds/0-people.js
+++ server/db/seeds/0-people.js
@@ -1,100 +1,101 @@
+exports.people = [
+  {
+    "id": 1,
+    "name": "Ali",
+    "image_url": "https://ca.slack-edge.com/T02SQPVAC-U023P86PY3B-976e2618adec-192"
+  },
+  {
+    "id": 2,
+    "name": "Austin",
+    "image_url": "https://avatars.githubusercontent.com/u/85047232?v=4"
+  },
+  {
+    "id": 3,
+    "name": "Kieran",
+    "image_url": "https://avatars.githubusercontent.com/u/72641947?v=4"
+  },
+  {
+    "id": 4,
+    "name": "Jessie",
+    "image_url": "https://avatars.githubusercontent.com/u/58745625?v=4"
+  },
+  {
+    "id": 5,
+    "name": "Prabhnoor",
+    "image_url": "https://ca.slack-edge.com/T02SQPVAC-U02548MA8BG-6727d8a9b7de-512"
+  },
+  {
+    "id": 6,
+    "name": "JV",
+    "image_url": "https://avatars.githubusercontent.com/u/83190123?v=4"
+  },
+  {
+    "id": 7,
+    "name": "Alan",
+    "image_url": "https://avatars.githubusercontent.com/u/85937572?v=4"
+  },
+  {
+    "id": 8,
+    "name": "Louis",
+    "image_url": "https://avatars.githubusercontent.com/u/62978181?v=4"
+  },
+  {
+    "id": 9,
+    "name": "Cris",
+    "image_url": "https://avatars.githubusercontent.com/u/80133173?v=4"
+  },
+  {
+    "id": 10,
+    "name": "Csiyang",
+    "image_url": "https://avatars.githubusercontent.com/u/67619008?v=4"
+  },
+  {
+    "id": 11,
+    "name": "James",
+    "image_url": "https://avatars.githubusercontent.com/u/80490743?v=4"
+  },
+  {
+    "id": 12,
+    "name": "Alex",
+    "image_url": "https://avatars.githubusercontent.com/u/85384120?v=4"
+  },
+  {
+    "id": 13,
+    "name": "Dianne",
+    "image_url": "https://avatars.githubusercontent.com/u/85599717?v=4"
+  },
+  {
+    "id": 14,
+    "name": "Dave",
+    "image_url": "https://ca.slack-edge.com/T02SQPVAC-U01THMGCFHD-af1da43ed094-512"
+  },
+  {
+    "id": 15,
+    "name": "Sophia",
+    "image_url": "https://avatars.githubusercontent.com/u/85972272?v=4"
+  },
+  {
+    "id": 16,
+    "name": "Tadhg",
+    "image_url": "https://avatars.githubusercontent.com/u/83738717?v=4"
+  },
+  {
+    "id": 17,
+    "name": "Zahira",
+    "image_url": "https://avatars.githubusercontent.com/u/86027651?v=4"
+  },
+  {
+    "id": 18,
+    "name": "Caleb",
+    "image_url": "https://www.history.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTc2MzAyNDY4NjM0NzgwODQ1/joe-biden-gettyimages-1267438366.jpg"
+  }
+]
 
 exports.seed = function (knex) {
   // Deletes ALL existing entries
   return knex('people').del()
     .then(function () {
       // Inserts seed entries
-      return knex('people').insert([
-        {
-          "id": 1,
-          "name": "Ali",
-          "image_url": "https://ca.slack-edge.com/T02SQPVAC-U023P86PY3B-976e2618adec-192"
-        },
-        {
-          "id": 2,
-          "name": "Austin",
-          "image_url": "https://avatars.githubusercontent.com/u/85047232?v=4"
-        },
-        {
-          "id": 3,
-          "name": "Kieran",
-          "image_url": "https://avatars.githubusercontent.com/u/72641947?v=4"
-        },
-        {
-          "id": 4,
-          "name": "Jessie",
-          "image_url": "https://avatars.githubusercontent.com/u/58745625?v=4"
-        },
-        {
-          "id": 5,
-          "name": "Prabhnoor",
-          "image_url": "https://ca.slack-edge.com/T02SQPVAC-U02548MA8BG-6727d8a9b7de-512"
-        },
-        {
-          "id": 6,
-          "name": "JV",
-          "image_url": "https://avatars.githubusercontent.com/u/83190123?v=4"
-        },
-        {
-          "id": 7,
-          "name": "Alan",
-          "image_url": "https://avatars.githubusercontent.com/u/85937572?v=4"
-        },
-        {
-          "id": 8,
-          "name": "Louis",
-          "image_url": "https://avatars.githubusercontent.com/u/62978181?v=4"
-        },
-        {
-          "id": 9,
-          "name": "Cris",
-          "image_url": "https://avatars.githubusercontent.com/u/80133173?v=4"
-        },
-        {
-          "id": 10,
-          "name": "Csiyang",
-          "image_url": "https://avatars.githubusercontent.com/u/67619008?v=4"
-        },
-        {
-          "id": 11,
-          "name": "James",
-          "image_url": "https://avatars.githubusercontent.com/u/80490743?v=4"
-        },
-        {
-          "id": 12,
-          "name": "Alex",
-          "image_url": "https://avatars.githubusercontent.com/u/85384120?v=4"
-        },
-        {
-          "id": 13,
-          "name": "Dianne",
-          "image_url": "https://avatars.githubusercontent.com/u/85599717?v=4"
-        },
-        {
-          "id": 14,
-          "name": "Dave",
-          "image_url": "https://ca.slack-edge.com/T02SQPVAC-U01THMGCFHD-af1da43ed094-512"
-        },
-        {
-          "id": 15,
-          "name": "Sophia",
-          "image_url": "https://avatars.githubusercontent.com/u/85972272?v=4"
-        },
-        {
-          "id": 16,
-          "name": "Tadhg",
-          "image_url": "https://avatars.githubusercontent.com/u/83738717?v=4"
-        },
-        {
-          "id": 17,
-          "name": "Zahira",
-          "image_url": "https://avatars.githubusercontent.com/u/86027651?v=4"
-        },
-        {
-          "id": 18,
-          "name": "Caleb",
-          "image_url": "https://www.history.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTc2MzAyNDY4NjM0NzgwODQ1/joe-biden-gettyimages-1267438366.jpg"
-        }
-      ]);
+      return knex('people').insert(people);
     });
 };
