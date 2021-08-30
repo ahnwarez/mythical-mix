f7e07cb7efbcdfb0dd379bb02f58587a655b0359
ahmad <eng.ahmed.anwar@gmail.com>
2021-08-31 06:41:51 +1200

    format code

--- client/mythical-mix.js
+++ client/mythical-mix.js
@@ -1,37 +1,30 @@
-export function assignGroups(peoplesNamesArra) {
-  //console.log(peoplesNamesArray);
+export function assignGroups (peoplesNamesArra) {
+  // console.log(peoplesNamesArray);
   const peoplesNamesArray = shuffleArray(peoplesNamesArra)
 
   const teams = [[], [], [], []]// push peoples names into array
   const length = peoplesNamesArray.length
   const remainder = length % 4
   for (let i = 0; i < length; i++) {
-
-
-    let num = Math.floor(Math.random() * teams.length);
+    const num = Math.floor(Math.random() * teams.length)
     if (teams[num].length < 4) {
       teams[num].push(peoplesNamesArray[i])
-    }
-
-
-    else {
+    } else {
       if (i >= length - remainder) {
         if (teams[num].length !== 5) {
           teams[num].push(peoplesNamesArray[i])
-        }
-        else {
-          let newNum = Math.floor(Math.random() * teams.length);
+        } else {
+          let newNum = Math.floor(Math.random() * teams.length)
           while (newNum === num) {
-            newNum = Math.floor(Math.random() * teams.length);
+            newNum = Math.floor(Math.random() * teams.length)
           }
           if (teams[newNum].length !== 5) {
             teams[newNum].push(peoplesNamesArray[i])
           }
         }
-      }
-      else {
+      } else {
         for (let j = 0; j < teams.length; j++) {
-          let element = teams[j]
+          const element = teams[j]
           if (element.length < 4) {
             element.push(peoplesNamesArray[i])
             break
@@ -39,22 +32,20 @@ export function assignGroups(peoplesNamesArra) {
         }
       }
     }
-  } return teams // last two will always be in bigger groups 
+  } return teams // last two will always be in bigger groups
   //
 
-  //HAVE AN ARRAY THAT HAS RANDOMLY PLACED INDEXES AND GO THROUGH IT NORMALLY AND RUN THE SAME CODE
-
+  // HAVE AN ARRAY THAT HAS RANDOMLY PLACED INDEXES AND GO THROUGH IT NORMALLY AND RUN THE SAME CODE
 }
-function shuffleArray(array) {
+function shuffleArray (array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
+    [array[i], array[j]] = [array[j], array[i]]
   }
   return array
 }
 
-
-export function assignGroupNames(team, teamsArray) {// jsonTeam is "team" key 
+export function assignGroupNames (team, teamsArray) { // jsonTeam is "team" key
   return teamsArray
     .map((people, i) => ({
       teamName: team[i],
